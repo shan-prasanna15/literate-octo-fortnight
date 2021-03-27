a2a7ea09686900e93087d186747c71479fe57008
shan.prasanna15 <shan.prasanna15@gmail.com>
2021-03-27 14:05:08 -0400

    Added Header and Myinfo

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+function Header(){
+    return (
+        <div>
+            <header className="App-header">
+                <h3>
+                    Introduction to myself
+                </h3>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import MyInfo from './MyInfo/MyInfo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <MyInfo />
+    <App />    
   </React.StrictMode>,
   document.getElementById('root')
 );
