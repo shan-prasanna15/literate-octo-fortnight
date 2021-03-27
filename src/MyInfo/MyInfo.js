8c31937a4660cdc4f428bcaed035045883ff70fe
Prasanna Shan <sporigins@gmail.com>
2021-03-27 12:34:03 -0400

    Altering list

--- src/MyInfo/MyInfo.js
+++ src/MyInfo/MyInfo.js
@@ -5,10 +5,11 @@ function MyInfo() {
     <div className="App">
       <h3>I am Prasanna</h3>
       <ul>
-          <li>I always understand everything</li>
-          <li>I always Initiate awesome things</li>
-          <li>I always Finish things succesfully on time</li>
-          <li>I always keep learning and apply them </li>
+          <li>Test Developer</li>
+          <li>Software Engineer</li>
+          <li>BSc Hons in Information Technology</li>          
+          <li>Microsoft Azure Fundamentals</li>
+          <li>.Net Brindging program - Humber College</li>
       </ul>
     </div>
   );
