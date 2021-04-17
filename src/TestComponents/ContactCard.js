8c7a9e5823ba3aa8157cb93fb883967a8b22fbd3
shan.prasanna15 <shan.prasanna15@gmail.com>
2021-04-17 02:09:24 -0400

    ContactCardObj to display multiple components

---
+++ src/TestComponents/ContactCard.js
@@ -0,0 +1,16 @@
+import React from "react"
+import ContactCardObj from './ContactCardObj'
+
+function ContactCard() {
+    return (
+        <div>
+            <ContactCardObj />
+            <ContactCardObj />
+            <ContactCardObj />
+            <ContactCardObj />
+        </div>
+        
+    )
+}
+
+export default ContactCard
---
+++ src/TestComponents/ContactCardObj.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './../App.css'
+
+function ContactCardObj(){
+    return (
+        <div className = 'App'>
+            <img src="http://placekitten.com/300/200"/>
+            <h3>Mr. Whiskerson</h3>
+            <p>Phone: (212) 555-1234</p>
+            <p>Email: mr.whiskaz@catnap.meow</p>
+        </div>
+    )
+}
+
+export default ContactCardObj;
